b0735ecc28d6acbae5135a60724d06fd6a311151
Elijah <elijahvw27@outlook.com>
2026-02-15 10:10:56 -0500

    update

--- infra/terraform/modules/rds_postgres/main.tf
+++ infra/terraform/modules/rds_postgres/main.tf
@@ -32,7 +32,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "15.4"
+  engine_version       = "15" # Changed to 15 to pick latest minor version
   instance_class       = "db.t3.micro"
   db_name              = "propertyflow"
   username             = "postgres"
