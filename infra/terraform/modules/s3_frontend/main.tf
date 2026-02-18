20ee6499a86ae44a74f12644911d221dd1ab6fc9
Elijah <elijahvw27@outlook.com>
2026-02-17 21:21:24 -0500

    push

--- infra/terraform/modules/s3_frontend/main.tf
+++ infra/terraform/modules/s3_frontend/main.tf
@@ -25,41 +25,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_id                = "S3-${aws_s3_bucket.frontend.id}"
   }
 
-  origin {
-    domain_name = var.alb_dns_name
-    origin_id   = "ALB-${var.alb_dns_name}"
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
-  }
-
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
-
-  ordered_cache_behavior {
-    path_pattern     = "/api/*"
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "ALB-${var.alb_dns_name}"
-
-    forwarded_values {
-      query_string = true
-      headers      = ["Authorization", "Origin", "Accept", "X-Company-ID"]
-      cookies {
-        forward = "all"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
@@ -121,7 +89,6 @@ resource "aws_s3_bucket_policy" "frontend" {
 }
 
 variable "environment" { type = string }
-variable "alb_dns_name" { type = string }
 
 output "bucket_name" { value = aws_s3_bucket.frontend.id }
 output "cloudfront_domain_name" { value = aws_cloudfront_distribution.s3_distribution.domain_name }
