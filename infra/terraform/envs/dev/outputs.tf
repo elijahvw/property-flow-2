bb01124c988579c4f722a2ea0b5a482329ac6922
Elijah <elijahvw27@outlook.com>
2026-02-18 13:32:58 -0500

    push

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -5,3 +5,19 @@ output "cloudfront_url" {
 output "cloudfront_distribution_id" {
   value = module.s3_frontend.cloudfront_distribution_id
 }
+
+output "repository_url" {
+  value = module.ecr.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = module.ecr.ecs_cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecr.ecs_service_name
+}
+
+output "alb_dns_name" {
+  value = module.ecr.alb_dns_name
+}
