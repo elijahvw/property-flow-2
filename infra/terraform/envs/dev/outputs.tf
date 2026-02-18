d173ecd925d3e7d50c65ae99eef8ce059ea3debc
Elijah <elijahvw27@outlook.com>
2026-02-17 23:42:13 -0500

    rem

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -5,23 +5,3 @@ output "cloudfront_url" {
 output "cloudfront_distribution_id" {
   value = module.s3_frontend.cloudfront_distribution_id
 }
-
-output "api_url" {
-  value = "http://${module.ecr.alb_dns_name}"
-}
-
-output "db_endpoint" {
-  value = module.rds.db_endpoint
-}
-
-output "cognito_domain" {
-  value = "https://${module.cognito.domain}.auth.${var.aws_region}.amazoncognito.com"
-}
-
-output "cognito_client_id" {
-  value = aws_cognito_user_pool_client.client.id
-}
-
-output "cognito_user_pool_id" {
-  value = module.cognito.user_pool_id
-}
